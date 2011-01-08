27583045e89577f6d128e7add1d91f0beef98e6e
Victor Bilyk <victorbilyk@gmail.com>
2011-01-08 15:56:34 +0600

    coffeescript 1.0 built with rhino 1.6R7 to prevent collisions with yuicompressor

--- project/build/CoffeeScriptSbtPluginProject.scala
+++ project/build/CoffeeScriptSbtPluginProject.scala
@@ -1,7 +1,10 @@
 import sbt._
 
 class CoffeeScriptSbtPluginProject(info: ProjectInfo) extends PluginProject(info) with test.ScalaScripted {
-  val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "1.0" from "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-1.0.jar"
+  //val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "1.0" from "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-1.0.jar"
+
+  val rhino = "rhino" % "js" % "1.6R7"
+
 
   override def scriptedSbt = "0.7.4"
   override def scriptedBufferLog = false
